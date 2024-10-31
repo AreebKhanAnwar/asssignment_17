7c1d251e606f66c1c15a3e200f9f8c6ad40011de
AreebKhanAnwar <areebkhanp1@gmail.com>
2024-10-31 20:57:57 +0500

    Add files via upload

---
+++ assign_17.js
@@ -0,0 +1,71 @@
+// Question no.1
+
+
+
+
+        // function power() {
+        //     var a = prompt("Enter a Number");
+        //     var b = prompt("Enter a power");
+
+        //     if (a == 0 && b == 0) {
+        //         console.log(" ");
+        //     } else {
+                
+        //       console.log(Math.pow(a, b));
+               
+        //     }
+        // }
+
+
+
+
+
+    // Question no.2
+
+
+
+
+
+//     function isLeapYear(year) {
+//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// const yearInput = prompt("Enter a year:");
+// const year = parseInt(yearInput);
+
+// if (isLeapYear(year)) {
+//     console.log(`${year} is a leap year.`);
+// } else {
+//     console.log(`${year} is not a leap year.`);
+// }
+
+
+
+
+// question no.3
+
+
+
+
+// function calculateSemiPerimeter(a, b, c) {
+//     return (a + b + c) / 2;
+// }
+
+// function calculateArea(a, b, c) {
+//     const S = calculateSemiPerimeter(a, b, c);
+//     return Math.sqrt(S * (S - a) * (S - b) * (S - c));
+// }
+
+// const a = parseFloat(prompt("Enter the length of side a:"));
+// const b = parseFloat(prompt("Enter the length of side b:"));
+// const c = parseFloat(prompt("Enter the length of side c:"));
+
+// if (a + b > c && a + c > b && b + c > a) { 
+//     const area = calculateArea(a, b, c);
+//     console.log(`The area of the triangle is: ${area}`);
+// } else {
+//     console.log("The lengths provided do not form a valid triangle.");
+// }
